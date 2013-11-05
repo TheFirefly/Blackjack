5cce1f7ef74bc3e3408718524a7196176fc735be
Ouellet <spaouellet@me.com>
2013-11-05 09:58:38 -0700

    Improved dealer calculation to account for player's score.

--- Blackjack.java
+++ Blackjack.java
@@ -5,6 +5,7 @@ public class Blackjack extends Applet {
 
 	private Deck deck;
 	private Player p;
+	private Player dealer;
 
 	public void init() {
 
@@ -13,14 +14,26 @@ public class Blackjack extends Applet {
 		deck.shuffle();
 
 		p = new Player("Player", deck.dealCard(), 17);
+		dealer = new Player("Dealer", deck.dealCard(), 17);
 
-		while(p.getScore() < 17) {
+		while(p.getScore() < p.getMaxScore()) {
 			p.dealCard(deck);
+		}
+
+		if (p.getScore() > 21) {
+			dealer.setMaxScore(11);
+		} else {
+			dealer.setMaxScore(p.getScore());
+		}
+
+		while(dealer.getScore() < dealer.getMaxScore()) {
+			dealer.dealCard(deck);
 		}
 	}
 
 	public void paint(Graphics g) {
-		p.getHand().draw(g);
+		p.getHand().draw(50, g);
+		dealer.getHand().draw(375, g);
 	}
 
 }
