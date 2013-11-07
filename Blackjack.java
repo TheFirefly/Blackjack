10a811cc66f6f2863944fd7d09b5260e08f32f41
Ouellet <spaouellet@me.com>
2013-11-07 08:12:02 -0700

    Draws shuffled and unshuffled decks.

--- Blackjack.java
+++ Blackjack.java
@@ -4,36 +4,43 @@ import java.applet.*;
 public class Blackjack extends Applet {
 
 	private Deck deck;
+	private Deck deck2;
 	private Player p;
 	private Player dealer;
 
 	public void init() {
 
 		this.deck = new Deck();
+		this.deck2 = new Deck();
 
 		deck.shuffle();
 
-		p = new Player("Player", deck.dealCard(), 17);
+		p = new Player("Player", deck.dealCard(), 20);
 		dealer = new Player("Dealer", deck.dealCard(), 17);
 
-		while(p.getScore() < p.getMaxScore()) {
+		p.dealCard(deck);
+		while (p.getScore() < p.getMaxScore()) {
 			p.dealCard(deck);
 		}
 
 		if (p.getScore() > 21) {
 			dealer.setMaxScore(11);
-		} else {
-			dealer.setMaxScore(p.getScore());
 		}
 
-		while(dealer.getScore() < dealer.getMaxScore()) {
+		dealer.dealCard(deck);
+		while (dealer.getScore() < dealer.getMaxScore()) {
 			dealer.dealCard(deck);
 		}
 	}
 
 	public void paint(Graphics g) {
-		p.getHand().draw(50, g);
-		dealer.getHand().draw(375, g);
+
+		deck.draw(g, 375);
+		deck2.draw(g, 50);
+
+
+		// p.getHand().draw(50, g);
+		// dealer.getHand().draw(375, g);
 	}
 
 }
